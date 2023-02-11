aefb3f880029e61ef2074d64e0774b77dbf313bb
ElenRi <m.ark2012@yandex.ru>
2023-02-11 04:52:56 +0300

    Добавила json, пагинацию

--- DjangoCountriess/urls.py
+++ DjangoCountriess/urls.py
@@ -21,7 +21,9 @@ from MainApp import views
 
 urlpatterns = [
     path('', views.main_page),
-    path('countries-page/<country>', views.countries_page),
-    path('countries-list', views.countries_list),
-    path('countries-languages', views.countries_languages),
+    path('countries-list/', views.countries_list),
+    path('languages-list/', views.languages),
+    path('languages-list/<str:language>', views.languages_countries),
+    path('countries-list/<str:country_name>', views.country),
+    path('countries-list/speaking-countries/<str:language>', views.language_in_countries)
 ]
