710f2c15387ad6d63fb2300f55f257bede50946a
ElenRi <m.ark2012@yandex.ru>
2023-02-18 09:42:54 +0300

    БД

--- DjangoCountriess/asgi.py
+++ DjangoCountriess/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DjangoCountriess project.
+ASGI config for DjangoCountriesss project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoCountriess.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoCountriesss.settings')
 
 application = get_asgi_application()
