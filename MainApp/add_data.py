897d34365cf68c85de5a682e9dbcaf7626b32f6f
ElenRi <m.ark2012@yandex.ru>
2023-02-18 12:44:16 +0300

    V2

---
+++ MainApp/add_data.py
@@ -0,0 +1,12 @@
+import json
+from MainApp.models import Countries
+
+file_path = '/home/student/Projects/DjangoCountries/country-by-languages.json'
+with open(file_path, 'r') as in_file:
+    data = json.load(in_file)
+
+for el in data:
+    cnt = el['country']
+    lang = ','.join(el['languages'])
+    cntr = Countries(country=cnt, languages=lang)
+    cntr.save()
