f6c115f120f8285bb56b49fa93ce9651651b4a5e
ElenRi <m.ark2012@yandex.ru>
2023-02-18 12:19:52 +0300

    Revert "Revert "Revert "Merge pull request #2 from ElenRi/master"""
    This reverts commit f836c3a8ef2bf739273d29f80cf66ed96a828aee.

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,5 +1,3 @@
 from django.db import models
 
-class Countries(models.Model):
-    country = models.CharField(max_length=50)
-    languages = models.CharField(max_length=500)
+# Create your models here.
